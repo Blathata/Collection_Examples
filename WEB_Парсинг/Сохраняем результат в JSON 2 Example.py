1fe0df774b280353c31d76e37ad1caa09c80f181
Brezhnev Ivan <IBrezhnev@mosbrew.ru>
2024-11-12 03:11:01 +0300

    add 1111

--- WEB_Парсинг/Сохраняем результат в JSON 2 Example.py
+++ WEB_Парсинг/Сохраняем результат в JSON 2 Example.py
@@ -32,21 +32,23 @@ category_page = [i.get('href') for i in [i for i in soup1.find('div', class_="na
 lstresult_json = []
 
 
-
-for i in range(1, len(pages)+1):
-    soup2 = soup(f'https://parsinger.ru/html/index{i}_page_{i}.html')
-    name = [i.text.strip() for i in soup2.find_all('a', class_="name_item")]
-    descriptions = [i.text.strip().split('\n') for i in soup2.find_all('div', class_="description")]
-    rate = [i.text.strip() for i in soup2.find_all('p', class_="price")]
-    for title, descr, price in zip(name, descriptions, rate):
-        lstresult_json.append({
-        "Наименование": title,
-        "Бренд": descr[0].split(':')[1].strip(),
-        "Форм-фактор": descr[1].split(':')[1].strip(),
-        "Ёмкость": descr[2].split(':')[1].strip(),
-        "Объем буферной памяти": descr[3].split(':')[1].strip(),
-        "Цена": price
-        })
+for cat_page in range(1, len(category_page)+1):
+    for i in range(1, len(pages)+1):
+        soup2 = soup(f'https://parsinger.ru/html/index{cat_page}_page_{i}.html')
+        name = [i.text.strip() for i in soup2.find_all('a', class_="name_item")]
+        descriptions = [i.text.strip().split('\n') for i in soup2.find_all('div', class_="description")]
+        rate = [i.text.strip() for i in soup2.find_all('p', class_="price")]
+        for title, descr, price in zip(name, descriptions, rate):
+            lstresult_json.append({
+            "Наименование": title,
+            descr[0].split(':')[0].strip(): descr[0].split(':')[1].strip(),
+            descr[1].split(':')[0].strip(): descr[1].split(':')[1].strip(),
+            descr[2].split(':')[0].strip(): descr[2].split(':')[1].strip(),
+            descr[3].split(':')[0].strip(): descr[3].split(':')[1].strip(),
+            "Цена": price
+            })
 
 with open('res.json', 'w', encoding='utf-8') as file:
     json.dump(lstresult_json, file, indent=4, ensure_ascii=False)
+
+print('Сбор данных окончен')
